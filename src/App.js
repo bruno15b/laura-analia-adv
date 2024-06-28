c8e82cefb5ae4016db82c0dc4dad15375826b2ee
Bruno Machado <brunomachado3818@gmail.com>
2024-06-27 21:51:47 -0300

    first test pipe deploy prod

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import "./App.css";
 function App() {
   return (
     <div className="fundo">
-      <header style={{ margin: "0 auto", height: "1vh", width: "1vw" }}>Site laura advogada.</header>
+      <header className="header">Site laura advogada.</header>
+      <section className="firstSection">
+        <div className="left"></div>
+        <div className="right">
+          <img src={`${process.env.PUBLIC_URL}/img/laura_adv.png`} alt="" />
+        </div>
+      </section>
     </div>
   );
 }
